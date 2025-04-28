17307f4b9b43139088aef60fbb1ea38c7b644c03
Pumbkinpat <Ptuan0898@gmail.com>
2025-04-28 19:50:30 +0700

    React Props

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Hello from "./components/Hello";
+
+function App() {
+  return (
+    <div className="App" >
+      <Hello name="pat" message="hell" />
+      <Hello name="navy" message="good morning" />
+      <Hello name="andree" message="hell nah" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Hello.jsx
@@ -0,0 +1,6 @@
+function Hello(bla) {
+    return <h1> {bla.name} {bla.message} </h1>
+}
+
+export default Hello;
+
