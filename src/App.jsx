d95f71b7f2fbb5c40b16e2725695973ddec55575
Pumbkinpat <Ptuan0898@gmail.com>
2025-04-28 19:52:31 +0700

    Css Styles
    	modified:   src/main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
 import Hello from "./components/Hello";
 
-function App() {
+const Card = ({title, rating}) => {
   return (
-    <div className="App" >
-      <Hello name="pat" message="hell" />
-      <Hello name="navy" message="good morning" />
-      <Hello name="andree" message="hell nah" />
+    <div className="card" style = {{
+      border: '1px solid #4b5362',
+      padding: '20px',
+      margin: '10px',
+      backgroundColor: '#31363f',
+      borderRadius: '10px',
+      minHeight: '100px'
+    }}>
+      <h2>{title}</h2>
+      <p>{rating}</p>
+      <br />
+    </div>
+  );
+}
+
+
+const App = () => {
+  return (
+    <div className="card-container">
+
+      <Card title={"Star War"} rating={5} isCool={true} />
+      <Card title={"Avatar"} rating={5}  />
+      <Card title={"The Lion King"} rating={4}  />
+
     </div>
   );
 }
